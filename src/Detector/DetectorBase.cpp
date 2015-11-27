2a5bdaf8a26034bf1098c98dea4321443a700005
Kai <kai.yan@labromance.com>
2015-11-27 19:43:22 +0900

    Added DetectorThrean base class

---
+++ src/Detector/DetectorBase.cpp
@@ -0,0 +1,21 @@
+/*
+ * DetectorBase.cpp
+ *
+ *  Created on: Nov 27, 2015
+ *      Author: root
+ */
+
+#include "DetectorBase.h"
+
+namespace kai
+{
+DetectorBase::DetectorBase() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DetectorBase::~DetectorBase() {
+	// TODO Auto-generated destructor stub
+}
+
+}
---
+++ src/Detector/DetectorBase.h
@@ -0,0 +1,34 @@
+/*
+ * DetectorBase.h
+ *
+ *  Created on: Nov 27, 2015
+ *      Author: root
+ */
+
+#ifndef DETECTOR_DETECTORBASE_H_
+#define DETECTOR_DETECTORBASE_H_
+
+#include "../Camera/CamFrame.h"
+
+namespace kai
+{
+
+class DetectorBase
+{
+public:
+	DetectorBase();
+	virtual ~DetectorBase();
+
+
+public:
+	CamFrame*	m_pFrame;
+	CamFrame*	m_pGrayFrame;
+
+
+};
+
+
+
+}
+
+#endif /* DETECTOR_DETECTORBASE_H_ */
