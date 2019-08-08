75f54ca0dfe7ce3c3ae1654554343be5ecc01484
Kai Yan <kai.yan@labromance.com>
2019-08-08 14:35:26 +0900

    Updated OpenPose

--- src/Detector/_OpenPose.cpp
+++ src/Detector/_OpenPose.cpp
@@ -14,6 +14,7 @@ _OpenPose::_OpenPose()
 	m_bSwapRB = false;
 	m_vMean.init();
 	m_scale = 1.0 / 255.0;
+	m_thr = 0.1;
 
 	m_iBackend = dnn::DNN_BACKEND_OPENCV;
 	m_iTarget = dnn::DNN_TARGET_CPU;
@@ -99,289 +100,61 @@ bool _OpenPose::detect(void)
 	m_fBGR.copy(*pBGR);
 	Mat mIn = *m_fBGR.m();
 
-	m_blob = blobFromImage(mIn, m_scale,
-			Size((int) ((368 * mIn.cols) / mIn.rows), 368),
+	m_blob = blobFromImage(mIn, m_scale, Size(m_nW, m_nH),
 			Scalar(m_vMean.x, m_vMean.y, m_vMean.z), m_bSwapRB, false);
 	m_net.setInput(m_blob);
 
-	cv::Mat netOutputBlob = m_net.forward();
+	Mat output = m_net.forward();
 
-	splitNetOutputBlobToParts(netOutputBlob, cv::Size(mIn.cols, mIn.rows), m_netOutputParts);
+	int H = output.size[2];
+	int W = output.size[3];
 
-	int keyPointId = 0;
-	for (int i = 0; i < OP_N_POINTS; ++i)
+	m_mDebug = mIn.clone();
+
+	// find the position of the body parts
+	vector<Point> points(OP_N_POINTS);
+	for (int n = 0; n < OP_N_POINTS; n++)
 	{
-		std::vector<KeyPoint> keyPoints;
-		getKeyPoints(m_netOutputParts[i], 0.1, keyPoints);
+		// Probability map of corresponding body's part.
+		Mat probMap(H, W, CV_32F, output.ptr(0, n));
 
-		std::cout << "Keypoints - " << keypointsMapping[i] << " : " << keyPoints << std::endl;
-
-		for (int i = 0; i < keyPoints.size(); ++i, ++keyPointId)
+		Point2f p(-1, -1);
+		Point maxLoc;
+		double prob;
+		minMaxLoc(probMap, 0, &prob, 0, &maxLoc);
+		if (prob > m_thr)
 		{
-			keyPoints[i].id = keyPointId;
-		}
+			p = maxLoc;
+			p.x *= (float) mIn.cols / W;
+			p.y *= (float) mIn.rows / H;
 
-		m_detectedKeypoints.push_back(keyPoints);
-		m_keyPointsList.insert(m_keyPointsList.end(), keyPoints.begin(), keyPoints.end());
+			circle(m_mDebug, cv::Point((int) p.x, (int) p.y), 8,
+					Scalar(0, 255, 255), -1);
+			cv::putText(m_mDebug, cv::format("%d", n),
+					cv::Point((int) p.x, (int) p.y), cv::FONT_HERSHEY_COMPLEX,
+					1, cv::Scalar(0, 0, 255), 2);
+
+		}
+		points[n] = p;
 	}
 
+	int nPairs = sizeof(POSE_PAIRS) / sizeof(POSE_PAIRS[0]);
+
+	for (int n = 0; n < nPairs; n++)
+	{
+		// lookup 2 connected body/hand parts
+		Point2f partA = points[POSE_PAIRS[n][0]];
+		Point2f partB = points[POSE_PAIRS[n][1]];
+
+		if (partA.x <= 0 || partA.y <= 0 || partB.x <= 0 || partB.y <= 0)
+			continue;
+
+		line(m_mDebug, partA, partB, Scalar(0, 255, 255), 8);
+		circle(m_mDebug, partA, 8, Scalar(0, 0, 255), -1);
+		circle(m_mDebug, partB, 8, Scalar(0, 0, 255), -1);
+	}
 
 	return true;
-}
-
-void _OpenPose::getKeyPoints(Mat& probMap, double threshold,
-		vector<KeyPoint>& keyPoints)
-{
-	Mat smoothProbMap;
-	GaussianBlur(probMap, smoothProbMap, Size(3, 3), 0, 0);
-
-	Mat maskedProbMap;
-	cv::threshold(smoothProbMap, maskedProbMap, threshold, 255, THRESH_BINARY);
-
-	maskedProbMap.convertTo(maskedProbMap, CV_8U, 1);
-
-	vector<vector<Point> > contours;
-	findContours(maskedProbMap, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
-
-	for (int i = 0; i < contours.size(); ++i)
-	{
-		Mat blobMask = Mat::zeros(smoothProbMap.rows, smoothProbMap.cols,
-				smoothProbMap.type());
-
-		fillConvexPoly(blobMask, contours[i], Scalar(1));
-
-		double maxVal;
-		Point maxLoc;
-
-		minMaxLoc(smoothProbMap.mul(blobMask), 0, &maxVal, 0, &maxLoc);
-
-		keyPoints.push_back(
-				KeyPoint(maxLoc, probMap.at<float>(maxLoc.y, maxLoc.x)));
-	}
-}
-
-void _OpenPose::populateColorPalette(vector<Scalar>& colors, int nColors)
-{
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> dis1(64, 200);
-	uniform_int_distribution<> dis2(100, 255);
-	uniform_int_distribution<> dis3(100, 255);
-
-	for (int i = 0; i < nColors; ++i)
-	{
-		colors.push_back(Scalar(dis1(gen), dis2(gen), dis3(gen)));
-	}
-}
-
-void _OpenPose::splitNetOutputBlobToParts(Mat& netOutputBlob,
-		const Size& targetSize, vector<Mat>& netOutputParts)
-{
-	int nParts = netOutputBlob.size[1];
-	int h = netOutputBlob.size[2];
-	int w = netOutputBlob.size[3];
-
-	for (int i = 0; i < nParts; ++i)
-	{
-		Mat part(h, w, CV_32F, netOutputBlob.ptr(0, i));
-
-		Mat resizedPart;
-
-		resize(part, resizedPart, targetSize);
-
-		netOutputParts.push_back(resizedPart);
-	}
-}
-
-void _OpenPose::populateInterpPoints(const Point& a, const Point& b,
-		int numPoints, vector<Point>& interpCoords)
-{
-	float xStep = ((float) (b.x - a.x)) / (float) (numPoints - 1);
-	float yStep = ((float) (b.y - a.y)) / (float) (numPoints - 1);
-
-	interpCoords.push_back(a);
-
-	for (int i = 1; i < numPoints - 1; ++i)
-	{
-		interpCoords.push_back(Point(a.x + xStep * i, a.y + yStep * i));
-	}
-
-	interpCoords.push_back(b);
-}
-
-void _OpenPose::getValidPairs(const vector<Mat>& netOutputParts,
-		const vector<vector<KeyPoint>>& detectedKeypoints,
-		vector<vector<ValidPair>>& validPairs, set<int>& invalidPairs)
-{
-	int nInterpSamples = 10;
-	float pafScoreTh = 0.1;
-	float confTh = 0.7;
-
-	for (int k = 0; k < mapIdx.size(); ++k)
-	{
-
-		//A->B constitute a limb
-		Mat pafA = netOutputParts[mapIdx[k].first];
-		Mat pafB = netOutputParts[mapIdx[k].second];
-
-		//Find the keypoints for the first and second limb
-		const vector<KeyPoint>& candA = detectedKeypoints[posePairs[k].first];
-		const vector<KeyPoint>& candB = detectedKeypoints[posePairs[k].second];
-
-		int nA = candA.size();
-		int nB = candB.size();
-
-		/*
-		 # If keypoints for the joint-pair is detected
-		 # check every joint in candA with every joint in candB
-		 # Calculate the distance vector between the two joints
-		 # Find the PAF values at a set of interpolated points between the joints
-		 # Use the above formula to compute a score to mark the connection valid
-		 */
-
-		if (nA != 0 && nB != 0)
-		{
-			vector<ValidPair> localValidPairs;
-
-			for (int i = 0; i < nA; ++i)
-			{
-				int maxJ = -1;
-				float maxScore = -1;
-				bool found = false;
-
-				for (int j = 0; j < nB; ++j)
-				{
-					pair<float, float> distance(
-							candB[j].point.x - candA[i].point.x,
-							candB[j].point.y - candA[i].point.y);
-
-					float norm = sqrt(
-							distance.first * distance.first
-									+ distance.second * distance.second);
-
-					if (!norm)
-					{
-						continue;
-					}
-
-					distance.first /= norm;
-					distance.second /= norm;
-
-					//Find p(u)
-					vector<Point> interpCoords;
-					populateInterpPoints(candA[i].point, candB[j].point,
-							nInterpSamples, interpCoords);
-					//Find L(p(u))
-					vector<pair<float, float>> pafInterp;
-					for (int l = 0; l < interpCoords.size(); ++l)
-					{
-						pafInterp.push_back(
-								pair<float, float>(
-										pafA.at<float>(interpCoords[l].y,
-												interpCoords[l].x),
-										pafB.at<float>(interpCoords[l].y,
-												interpCoords[l].x)));
-					}
-
-					vector<float> pafScores;
-					float sumOfPafScores = 0;
-					int numOverTh = 0;
-					for (int l = 0; l < pafInterp.size(); ++l)
-					{
-						float score = pafInterp[l].first * distance.first
-								+ pafInterp[l].second * distance.second;
-						sumOfPafScores += score;
-						if (score > pafScoreTh)
-						{
-							++numOverTh;
-						}
-
-						pafScores.push_back(score);
-					}
-
-					float avgPafScore = sumOfPafScores
-							/ ((float) pafInterp.size());
-
-					if (((float) numOverTh) / ((float) nInterpSamples) > confTh)
-					{
-						if (avgPafScore > maxScore)
-						{
-							maxJ = j;
-							maxScore = avgPafScore;
-							found = true;
-						}
-					}
-
-				}/* j */
-
-				if (found)
-				{
-					localValidPairs.push_back(
-							ValidPair(candA[i].id, candB[maxJ].id, maxScore));
-				}
-
-			}/* i */
-
-			validPairs.push_back(localValidPairs);
-
-		}
-		else
-		{
-			invalidPairs.insert(k);
-			validPairs.push_back(vector<ValidPair>());
-		}
-	}/* k */
-}
-
-void _OpenPose::getPersonwiseKeypoints(
-		const vector<vector<ValidPair>>& validPairs,
-		const set<int>& invalidPairs, vector<vector<int>>& personwiseKeypoints)
-{
-	for (int k = 0; k < mapIdx.size(); ++k)
-	{
-		if (invalidPairs.find(k) != invalidPairs.end())
-		{
-			continue;
-		}
-
-		const vector<ValidPair>& localValidPairs(validPairs[k]);
-
-		int indexA(posePairs[k].first);
-		int indexB(posePairs[k].second);
-
-		for (int i = 0; i < localValidPairs.size(); ++i)
-		{
-			bool found = false;
-			int personIdx = -1;
-
-			for (int j = 0; !found && j < personwiseKeypoints.size(); ++j)
-			{
-				if (indexA < personwiseKeypoints[j].size()
-						&& personwiseKeypoints[j][indexA]
-								== localValidPairs[i].aId)
-				{
-					personIdx = j;
-					found = true;
-				}
-			}/* j */
-
-			if (found)
-			{
-				personwiseKeypoints[personIdx].at(indexB) =
-						localValidPairs[i].bId;
-			}
-			else if (k < 17)
-			{
-				vector<int> lpkp(vector<int>(18, -1));
-
-				lpkp.at(indexA) = localValidPairs[i].aId;
-				lpkp.at(indexB) = localValidPairs[i].bId;
-
-				personwiseKeypoints.push_back(lpkp);
-			}
-
-		}/* i */
-	}/* k */
 }
 
 bool _OpenPose::draw(void)
@@ -392,42 +165,9 @@ bool _OpenPose::draw(void)
 	Mat* pMat = pFrame->m();
 	IF_F(pMat->empty());
 
-	std::vector<cv::Scalar> colors;
-	populateColorPalette(colors, OP_N_POINTS);
-
-	for (int i = 0; i < OP_N_POINTS; ++i)
+	if (!m_mDebug.empty())
 	{
-		for (int j = 0; j < m_detectedKeypoints[i].size(); ++j)
-		{
-			cv::circle(*pMat, m_detectedKeypoints[i][j].point, 5, colors[i], -1, cv::LINE_AA);
-		}
-	}
-
-	std::vector<std::vector<ValidPair>> validPairs;
-	std::set<int> invalidPairs;
-	getValidPairs(m_netOutputParts, m_detectedKeypoints, validPairs, invalidPairs);
-
-	std::vector<std::vector<int>> personwiseKeypoints;
-	getPersonwiseKeypoints(validPairs, invalidPairs, personwiseKeypoints);
-
-	for (int i = 0; i < OP_N_POINTS - 1; ++i)
-	{
-		for (int n = 0; n < personwiseKeypoints.size(); ++n)
-		{
-			const std::pair<int, int>& posePair = posePairs[i];
-			int indexA = personwiseKeypoints[n][posePair.first];
-			int indexB = personwiseKeypoints[n][posePair.second];
-
-			if (indexA == -1 || indexB == -1)
-			{
-				continue;
-			}
-
-			const KeyPoint& kpA = m_keyPointsList[indexA];
-			const KeyPoint& kpB = m_keyPointsList[indexB];
-
-			cv::line(*pMat, kpA.point, kpB.point, colors[i], 3, cv::LINE_AA);
-		}
+		imshow("Pose", m_mDebug);
 	}
 
 	return true;
--- src/Detector/_OpenPose.h
+++ src/Detector/_OpenPose.h
@@ -17,130 +17,38 @@ using namespace cv::dnn;
 namespace kai
 {
 
-struct KeyPoint
-{
-	KeyPoint(cv::Point point, float probability)
-	{
-		this->id = -1;
-		this->point = point;
-		this->probability = probability;
-	}
+//#define MPI
+#define COCO
 
-	int id;
-	cv::Point point;
-	float probability;
+#ifdef MPI
+const int POSE_PAIRS[14][2] =
+{
+    {0,1}, {1,2}, {2,3},
+    {3,4}, {1,5}, {5,6},
+    {6,7}, {1,14}, {14,8}, {8,9},
+    {9,10}, {14,11}, {11,12}, {12,13}
 };
 
-inline std::ostream& operator <<(std::ostream& os, const KeyPoint& kp)
-{
-	os << "Id:" << kp.id << ", Point:" << kp.point << ", Prob:"
-			<< kp.probability << std::endl;
-	return os;
-}
+string protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt";
+string weightsFile = "pose/mpi/pose_iter_160000.caffemodel";
 
-struct ValidPair
-{
-	ValidPair(int aId, int bId, float score)
-	{
-		this->aId = aId;
-		this->bId = bId;
-		this->score = score;
-	}
+#define OP_N_POINTS 15
+#endif
 
-	int aId;
-	int bId;
-	float score;
+#ifdef COCO
+const int POSE_PAIRS[17][2] =
+{
+    {1,2}, {1,5}, {2,3},
+    {3,4}, {5,6}, {6,7},
+    {1,8}, {8,9}, {9,10},
+    {1,11}, {11,12}, {12,13},
+    {1,0}, {0,14},
+    {14,16}, {0,15}, {15,17}
 };
-
-inline std::ostream& operator <<(std::ostream& os, const ValidPair& vp)
-{
-	os << "A:" << vp.aId << ", B:" << vp.bId << ", score:" << vp.score
-			<< std::endl;
-	return os;
-}
-
-template<class T> std::ostream& operator <<(std::ostream& os,
-		const std::vector<T>& v)
-{
-	os << "[";
-	bool first = true;
-	for (typename std::vector<T>::const_iterator ii = v.begin(); ii != v.end();
-			++ii, first = false)
-	{
-		if (!first)
-			os << ",";
-		os << " " << *ii;
-	}
-	os << "]";
-	return os;
-}
-
-template<class T> std::ostream& operator <<(std::ostream& os,
-		const std::set<T>& v)
-{
-	os << "[";
-	bool first = true;
-	for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end();
-			++ii, first = false)
-	{
-		if (!first)
-			os << ",";
-		os << " " << *ii;
-	}
-	os << "]";
-	return os;
-}
 
 #define OP_N_POINTS 18
 
-const std::string keypointsMapping[] =
-{ "Nose", "Neck", "R-Sho", "R-Elb", "R-Wr", "L-Sho", "L-Elb", "L-Wr", "R-Hip",
-		"R-Knee", "R-Ank", "L-Hip", "L-Knee", "L-Ank", "R-Eye", "L-Eye",
-		"R-Ear", "L-Ear" };
-
-const std::vector<std::pair<int, int>> mapIdx =
-{
-{ 31, 32 },
-{ 39, 40 },
-{ 33, 34 },
-{ 35, 36 },
-{ 41, 42 },
-{ 43, 44 },
-{ 19, 20 },
-{ 21, 22 },
-{ 23, 24 },
-{ 25, 26 },
-{ 27, 28 },
-{ 29, 30 },
-{ 47, 48 },
-{ 49, 50 },
-{ 53, 54 },
-{ 51, 52 },
-{ 55, 56 },
-{ 37, 38 },
-{ 45, 46 } };
-
-const std::vector<std::pair<int, int>> posePairs =
-{
-{ 1, 2 },
-{ 1, 5 },
-{ 2, 3 },
-{ 3, 4 },
-{ 5, 6 },
-{ 6, 7 },
-{ 1, 8 },
-{ 8, 9 },
-{ 9, 10 },
-{ 1, 11 },
-{ 11, 12 },
-{ 12, 13 },
-{ 1, 0 },
-{ 0, 14 },
-{ 14, 16 },
-{ 0, 15 },
-{ 15, 17 },
-{ 2, 17 },
-{ 5, 16 } };
+#endif
 
 class _OpenPose: public _DetectorBase
 {
@@ -154,20 +62,6 @@ public:
 	int check(void);
 
 private:
-	void getKeyPoints(Mat& probMap, double threshold,
-			vector<KeyPoint>& keyPoints);
-	void populateColorPalette(vector<Scalar>& colors, int nColors);
-	void splitNetOutputBlobToParts(Mat& netOutputBlob, const Size& targetSize,
-			vector<Mat>& netOutputParts);
-	void populateInterpPoints(const Point& a, const Point& b, int numPoints,
-			vector<Point>& interpCoords);
-	void getValidPairs(const vector<Mat>& netOutputParts,
-			const vector<vector<KeyPoint>>& detectedKeypoints,
-			vector<vector<ValidPair>>& validPairs, set<int>& invalidPairs);
-	void getPersonwiseKeypoints(const vector<vector<ValidPair>>& validPairs,
-			const set<int>& invalidPairs,
-			vector<vector<int>>& personwiseKeypoints);
-
 	bool detect(void);
 	void update(void);
 	static void* getUpdateThread(void* This)
@@ -183,18 +77,14 @@ public:
 	bool m_bSwapRB;
 	float m_scale;
 	vInt3 m_vMean;
+	float m_thr;
 	Mat m_blob;
 	vector<string> m_vLayerName;
 
 	int m_iBackend;
 	int m_iTarget;
 
-	std::vector<cv::Mat> m_netOutputParts;
-	std::vector<std::vector<KeyPoint>> m_detectedKeypoints;
-	std::vector<KeyPoint> m_keyPointsList;
-
-
-
+	Mat m_mDebug;
 };
 
 }
