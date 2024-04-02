408cebd09189b13cdebfebf514584c29b529ca0f
Kai Yan <kai.yan@labromance.com>
2024-04-02 11:44:23 +0900

    Updated .sh

---
+++ sh/Setup/openCL.sh
@@ -0,0 +1,7 @@
+#----------------------------------------------------
+# Intel
+
+#https://www.intel.com/content/www/us/en/developer/articles/tool/opencl-drivers.html
+https://github.com/intel/compute-runtime/releases
+
+sudo apt-get install clinfo
