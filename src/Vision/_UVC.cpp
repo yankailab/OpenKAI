528323e72a90be859a3a79eda004db71ac8ea61e
Kai Yan <kai.yan@labromance.com>
2025-02-17 04:14:49 +0900

    Updated _UVC

--- src/Vision/_UVC.cpp
+++ src/Vision/_UVC.cpp
@@ -51,6 +51,13 @@ namespace kai
 
 		IF_F(!UVCopen());
 
+		/* set stream type: temperature+yuv */
+		if (stream_type_config(STREAM_TYPE_YUV_TEMP, 20) != 0)
+		{
+			printf("\n--- stream_type_config error ---\n");
+			return false;
+		}
+
 		UVCsetVideoMode();
 		UVCstreamStart();
 
@@ -164,7 +171,8 @@ namespace kai
 			return false;
 		}
 
-		r = uvc_stream_start(m_pHandleStream, sCbGetFrame, this, 0);
+		//		r = uvc_stream_start(m_pHandleStream, sCbGetFrame, this, 0);
+		r = uvc_stream_start(m_pHandleStream, NULL, this, 0);
 		if (r != UVC_SUCCESS)
 		{
 			LOG_I("stream_start error!");
@@ -198,6 +206,16 @@ namespace kai
 		//		return (int)err;
 	}
 
+	int _UVC::UVCctrlTransfer(unsigned char bmRequestType, unsigned char bRequest,
+						 unsigned short wValue, unsigned short wIndex,
+						 unsigned char *data, uint16_t wLength, unsigned int timeout)
+	{
+		libusb_device_handle* pHandleUSB = uvc_get_libusb_handle(m_pHandleDev);
+
+		return libusb_control_transfer(pHandleUSB, bmRequestType, bRequest, wValue, wIndex,
+									   data, wLength, timeout);
+	}
+
 	int _UVC::start(void)
 	{
 		NULL__(m_pT, OK_ERR_NULLPTR);
@@ -220,6 +238,7 @@ namespace kai
 			m_pT->autoFPS();
 
 			Mat mCam;
+
 			// while (!m_UVC.read(mCam))
 			// 	;
 			// m_fRGB.copy(mCam);
@@ -229,6 +248,278 @@ namespace kai
 			// 	m_UVC.release();
 			// 	m_bOpen = false;
 			// }
+
+			uvc_frame_t *frame = nullptr;
+			uvc_error_t r;
+
+			// Get frame (blocking call)
+			r = uvc_stream_get_frame(m_pHandleStream, &frame, 1000000); // Timeout: 1 second
+			if (r < 0 || !frame)
+			{
+				std::cerr << "Failed to get frame: " << uvc_strerror(r) << std::endl;
+				continue;
+			}
+
+			if (frame->data_bytes != m_uvcSize)
+			{
+				std::cerr << "Frame size wrong" << std::endl;
+				continue;
+			}
+
+			Mat mRGB;
+			Mat mYUV = cv::Mat(m_vSizeRGB.y, m_vSizeRGB.x, CV_8UC2, ((unsigned char *)frame->data) + m_uvcOffset, Mat::AUTO_STEP);
+			cv::cvtColor(mYUV, mRGB, COLOR_YUV2RGB_YUY2);
+
+			m_fRGB.copy(mRGB);
 		}
 	}
+
+
+	int _UVC::get_len(unsigned short wValue, int retries)
+	{
+		int err, c = 0;
+		unsigned char data[2];
+
+		while (c < retries)
+		{
+			err = UVCctrlTransfer(REQ_TYPE_GET, REQ_GET_LEN, wValue, 0x0a00, data, 2, 0);
+			if (err == 2)
+				return *((unsigned short *)data);
+
+			printf("--- get_len error: %d ---\n", err);
+			++c;
+			usleep(500000);
+		}
+
+		return -1;
+	}
+
+	int _UVC::get_cur(unsigned short wValue, unsigned char *data, unsigned short len, int retries)
+	{
+		int err, c = 0;
+
+		while (c < retries)
+		{
+			err = UVCctrlTransfer(REQ_TYPE_GET, REQ_GET_CUR, wValue, 0x0a00, data, len, 0);
+			if (err == len)
+				return len;
+
+			printf("--- get_cur error: %d ---\n", err);
+			++c;
+			usleep(500000);
+		}
+
+		return -1;
+	}
+
+	int _UVC::get_errno(void)
+	{
+		int len;
+		unsigned char data[4];
+
+		len = get_len(XU_CS_ID_ERROR_CODE, 10);
+		if (len < 0)
+			return -1;
+		if (get_cur(XU_CS_ID_ERROR_CODE, data, 1, 10) < 0)
+			return -1;
+
+		printf("errno = %d\n", data[0]);
+		return (int)data[0];
+	}
+
+	int _UVC::set_cur(unsigned short wValue, unsigned char *data, unsigned short len, int retries)
+	{
+		int err, c = 0;
+
+		while (c < retries)
+		{
+			err = UVCctrlTransfer(REQ_TYPE_SET, REQ_SET_CUR, wValue, 0x0a00, data, len, 0);
+			if (err == len)
+				return len;
+			printf("--- set_cur error: %d ---\n", err);
+			++c;
+			usleep(500000);
+		}
+
+		return -1;
+	}
+
+	int _UVC::set_curr_func(unsigned short cs_id, unsigned char sub_id, int retries)
+	{
+		int len;
+		unsigned char func[2];
+
+		while (1)
+		{
+			len = get_len(XU_CS_ID_COMMAND_SWITCH, retries);
+			if (len == 2)
+				break;
+			printf("--- get switch cmd len error: %d. ---\n", len);
+			usleep(500000);
+		}
+
+		func[0] = (cs_id >> 8) & 0xff;
+		func[1] = sub_id;
+		if (set_cur(XU_CS_ID_COMMAND_SWITCH, func, 2, retries) < 0)
+		{
+			printf("--- set switch cmd error. ---\n");
+			return -1;
+		}
+
+		return 0;
+	}
+
+	int _UVC::set_curr_data(unsigned short cs_id, unsigned char *buf, int retries)
+	{
+		int len;
+
+		len = get_len(cs_id, retries);
+		if (len < 0)
+		{
+			printf("--- get_len error. ---\n");
+			return -1;
+		}
+		printf("\n--- set_curr_data: len = %d ---\n\n", len);
+		if (set_cur(cs_id, buf, len, retries) < 0)
+		{
+			printf("--- set_curr error. ---\n");
+			return -1;
+		}
+
+		return len;
+	}
+
+	int _UVC::get_curr_data(unsigned short cs_id, unsigned char *buf, int retries)
+	{
+		int len;
+
+		len = get_len(cs_id, retries);
+		if (len < 0)
+		{
+			printf("--- get_len error. ---\n");
+			return -1;
+		}
+
+		printf("\n--- get_curr_data: len = %d ---\n\n", len);
+
+		if (get_cur(cs_id, buf, len, retries) < 0)
+		{
+			printf("-- get_curr error. ---\n");
+			return -1;
+		}
+
+		return len;
+	}
+
+	int _UVC::get_protocol_version(int retries)
+	{
+		int len;
+		unsigned char data[4];
+
+		len = get_len(XU_CS_ID_PROTOCOL_VER, retries);
+		if (len < 0)
+		{
+			printf("--- get version len error. ---\n");
+			return -1;
+		}
+		printf("--- proto version len: %d ---\n", len);
+		if (get_cur(XU_CS_ID_PROTOCOL_VER, data, len, retries) < 0)
+		{
+			printf("--- get version error. ---\n");
+			return -1;
+		}
+		printf("\n--- proto version: %s ---\n\n", data);
+
+		return 0;
+	}
+
+	void _UVC::dump_devinfo(unsigned char *ptr)
+	{
+		printf("\nfirmwareVersion: %s\n", ptr);
+		ptr += 64;
+		printf("encoderVersion: %s\n", ptr);
+		ptr += 64;
+		printf("hardwareVersion: %s\n", ptr);
+		ptr += 64;
+		printf("deviceName: %s\n", ptr);
+		ptr += 64;
+		printf("protocolVersion: %s\n", ptr);
+		ptr += 4;
+		printf("serialNumber: %s\n\n", ptr);
+	}
+
+	unsigned int _UVC::get_fw_version(uint8_t *d)
+	{
+		char *ptr = (char *)d;
+
+		ptr = strstr(ptr, "BUILD");
+		ptr += 6;
+		return (unsigned int)strtol(ptr, NULL, 10);
+	}
+
+	int _UVC::get_device_info(int retries)
+	{
+		unsigned char rbuf[1600];
+		unsigned int fw_ver;
+
+		printf("\n--- starting get_device_info ----\n");
+		if (set_curr_func(XU_CS_ID_SYSTEM, SYSTEM_DEVICE_INFO, retries) < 0)
+			return -1;
+		if (get_curr_data(XU_CS_ID_SYSTEM, rbuf, retries) < 0)
+			return -1;
+		dump_devinfo(rbuf);
+		fw_ver = get_fw_version(rbuf);
+		printf("fw_ver = %u\n", fw_ver);
+
+		//	if (set_curr_func(u, XU_CS_ID_THERMAL, THERMAL_ALG_VERSION) < 0) return -1;
+		//	if (get_curr_data(u, XU_CS_ID_THERMAL, rbuf) < 0) return -1;
+		return 0;
+	}
+
+	int _UVC::wait_cmd_done(int after, int repeat)
+	{
+		int r = 0;
+
+		usleep(after * 1000);
+		while ((r = get_errno()) == 1)
+			usleep(repeat * 1000);
+
+		printf("finally errno = %d\n", r);
+		return r;
+	}
+
+	int _UVC::stream_type_config(unsigned char type, int retries)
+	{
+		unsigned char data[1600] = {0};
+
+		printf("\n--- starting stream_type_config ----\n");
+		if (set_curr_func(XU_CS_ID_THERMAL, THERMAL_STREAM_PARAM, retries) != 0)
+			return -1;
+		wait_cmd_done(15, 15);
+
+		if (get_curr_data(XU_CS_ID_THERMAL, data, retries) < 0)
+			return -1;
+		printf("0: chnlid = %d, stream_type = %d\n", data[0], data[1]);
+
+		if (data[1] != type)
+		{
+			data[1] = type;
+			if (set_curr_data(XU_CS_ID_THERMAL, data, retries) < 0)
+				return -1;
+
+			wait_cmd_done(100, 100);
+
+			get_curr_data(XU_CS_ID_THERMAL, data, retries);
+			printf("1: chnlid = %d, stream_type = %d\n", data[0], data[1]);
+		}
+		else
+		{
+			printf("---- stream_type meets my demands and not config again! ------\n");
+		}
+
+		return 0;
+	}
+
+
+
 }
--- src/Vision/_UVC.h
+++ src/Vision/_UVC.h
@@ -10,6 +10,59 @@
 
 #include "_VisionBase.h"
 #include <libuvc/libuvc.h>
+#include <libusb-1.0/libusb.h>
+
+///////////////////////////////////////////////////////
+/* bmRequestType */
+#define REQ_TYPE_SET 0x21
+#define REQ_TYPE_GET 0xa1
+
+/* bmRequest */
+#define REQ_SET_CUR 0x01
+#define REQ_GET_CUR 0x81
+#define REQ_GET_LEN 0x85
+
+/* wValue: CS ID */
+#define XU_CS_ID_SYSTEM (unsigned short)0x0100
+/* sub-function ID */
+#define SYSTEM_DEVICE_INFO (unsigned char)0x01
+#define SYSTEM_REBOOT (unsigned char)0x02
+#define SYSTEM_RESET (unsigned char)0x03
+#define SYSTEM_HARDWARE_SERVER (unsigned char)0x04
+#define SYSTEM_LOCALTIME (unsigned char)0x05
+#define SYSTEM_UPDATE_FIRMWARE (unsigned char)0x06
+#define SYSTEM_DIAGNOSED_DATA (unsigned char)0x07
+#define SYSTEM_UPDATE_STATE (unsigned char)0x08
+
+#define XU_CS_ID_IMAGE (unsigned short)0x0200
+#define IMAGE_BRIGHTNESS (unsigned char)0x01
+#define IMAGE_CONTRAST (unsigned char)0x02
+#define IMAGE_BACKGROUND_CORRECT (unsigned char)0x03
+#define IMAGE_MANUAL_CORRECT (unsigned char)0x04
+#define IMAGE_ENHANCEMENT (unsigned char)0x05
+#define IMAGE_VIDEO_ADJUST (unsigned char)0x06
+
+#define XU_CS_ID_THERMAL (unsigned short)0x0300
+#define THERMAL_BASIC_PARAM (unsigned char)0x01
+#define THERMAL_MODE (unsigned char)0x02
+#define THERMAL_MODE_NORMAL 1
+#define THERMAL_MODE_EXPERT 2
+
+#define THERMAL_ALG_VERSION (unsigned char)0x04
+#define THERMAL_STREAM_PARAM (unsigned char)0x05
+#define STREAM_TYPE_YUV_ONLY 6
+#define STREAM_TYPE_YUV_RAW 9
+#define STREAM_TYPE_YUV_TEMP 8
+
+#define THERMAL_CALIBRATION_FILE (unsigned char)0x0e
+#define THERMAL_EXPERT_REGIONS (unsigned char)0x0f
+#define THERMAL_EXPERT_CORRECTION_PARAM (unsigned char)0x10
+#define THERMAL_EXPERT_CORRECTION_START (unsigned char)0x11
+#define THERMAL_TEMP_RISE_CALIBRATION (unsigned char)0x12
+
+#define XU_CS_ID_PROTOCOL_VER (unsigned short)0x0400
+#define XU_CS_ID_COMMAND_SWITCH (unsigned short)0x0500
+#define XU_CS_ID_ERROR_CODE (unsigned short)0x0600
 
 #define UVC_FMT_ANY "any"
 #define UVC_FMT_UNCOMPRESSED "uncompressed"
@@ -44,13 +97,29 @@ namespace kai
 		};
 
 	private:
+		int get_len(unsigned short wValue, int retries);
+		int get_cur(unsigned short wValue, unsigned char *data, unsigned short len, int retries);
+		int get_errno(void);
+		int set_cur(unsigned short wValue, unsigned char *data, unsigned short len, int retries);
+		int set_curr_func(unsigned short cs_id, unsigned char sub_id, int retries);
+		int set_curr_data(unsigned short cs_id, unsigned char *buf, int retries);
+		int get_curr_data(unsigned short cs_id, unsigned char *buf, int retries);
+		int get_protocol_version(int retries);
+		void dump_devinfo(unsigned char *ptr);
+		unsigned int get_fw_version(uint8_t *d);
+		int get_device_info(int retries);
+		int wait_cmd_done(int after, int repeat);
+		int stream_type_config(unsigned char type, int retries);
+
 		bool UVCopen(void);
 		void UVCclose(void);
 		void UVCsetVideoMode(void);
 		bool UVCstreamStart(void);
 		void UVCstreamClose(void);
 		void UVCstreamGetFrame(unsigned int tOut);
-		void UVCctrlTransfer(void);
+		int UVCctrlTransfer(unsigned char bmRequestType, unsigned char bRequest,
+							unsigned short wValue, unsigned short wIndex,
+							unsigned char *data, uint16_t wLength, unsigned int timeout);
 		void cbGetFrame(uvc_frame *pFrame);
 
 		void update(void);
